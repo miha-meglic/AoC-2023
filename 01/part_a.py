cb4fbe698af2b0f98cd24dadd261c317e2bbb2d1
Miha Meglic <miha@meglic.dev>
2023-12-02 15:58:46 +0100

    Day 1 - Refactored to use advent-of-code-data

--- 01/part_a.py
+++ 01/part_a.py
@@ -1,11 +1,13 @@
+import aocd
 import re
+
+data = aocd.get_data(day=1, year=2023)
 
 total = 0
 
-with open("input.txt", "r") as f:
-	for line in f:
-		digits = re.findall(r'\d', line)
-		value = int(digits[0] + digits[-1])
-		total += value
+for line in data.splitlines():
+	digits = re.findall(r'\d', line)
+	value = int(digits[0] + digits[-1])
+	total += value
 
 print(total)
--- 01/part_b.py
+++ 01/part_b.py
@@ -1,6 +1,7 @@
 # You need to explicitly install the regex module with pip
 # This adds the `overlapped` flag to the findall method
 import regex as re
+import aocd
 
 digit_map = {
 	'one': '1',
@@ -16,11 +17,12 @@ digit_map = {
 
 total = 0
 
-with open("input.txt", "r") as f:
-	for line in f:
-		digits = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
-		digits = list(map(lambda x: digit_map[x] if len(x) > 1 else x, digits))
-		value = int(digits[0] + digits[-1])
-		total += value
+data = aocd.get_data(day=1, year=2023)
+
+for line in data.splitlines():
+	digits = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line, overlapped=True)
+	digits = list(map(lambda x: digit_map[x] if len(x) > 1 else x, digits))
+	value = int(digits[0] + digits[-1])
+	total += value
 
 print(total) 
